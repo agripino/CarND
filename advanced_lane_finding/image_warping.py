b19d41e10ca8fc22d752080a1de015b14f31cf46
Antonio Agripino <agripino.nunes@gmail.com>
2017-02-22 22:05:06 -0300

    New selection of perspective points

--- advanced_lane_finding/image_warping.py
+++ advanced_lane_finding/image_warping.py
@@ -10,8 +10,8 @@ def warp_image(bin_image):
     # Height
     h = bin_image.shape[0]
 
-    src = np.float32([[0.22*w, 0.965*h], [0.85*w, 0.965*h], [0.565*w, 0.632*h], [0.475*w, 0.632*h]])
-    dst = np.float32([[0.22*w, 0.965*h], [0.85*w, 0.965*h], [0.85*w, 0], [0.22*w, 0]])
+    src = np.float32([[0.2313*w, 0.9542*h], [0.8570*w, 0.9542*h], [0.5891*w, 0.6639*h], [0.4430*w, 0.6639*h]])
+    dst = np.float32([[0.2313*w, 0.9542*h], [0.8570*w, 0.9542*h], [0.8570*w, 0], [0.2313*w, 0]])
 
     M = cv2.getPerspectiveTransform(src, dst)
     warped_image = cv2.warpPerspective(bin_image, M, bin_image.shape[::-1])
