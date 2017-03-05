83e235197a12e01f45cf93fc31cce9bad4200582
Antonio Agripino <agripino.nunes@gmail.com>
2017-03-05 20:47:34 -0300

    CLAHE on RGB R channel

--- advanced_lane_finding/color_spaces.py
+++ advanced_lane_finding/color_spaces.py
@@ -7,6 +7,10 @@ def hls_s_thresh(img, thresh=(0, 255)):
     # Convert to HLS color space
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
 
+    # Create an object encapsulating the CLAHE parameters
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(50, 50))
+    hls[:, :, 2] = clahe.apply(hls[:, :, 2])
+
     # Create binary output mask
     binary_output = np.zeros_like(img[:, :, 0])
     binary_output[(hls[:, :, 2] >= thresh[0]) & (hls[:, :, 2] <= thresh[1])] = 1
@@ -15,10 +19,14 @@ def hls_s_thresh(img, thresh=(0, 255)):
 
 
 def rgb_r_thresh(img, thresh=(0, 255)):
-    # Image is assumed in BGR color space
+    # Image is assumed in RGB color space
+    # Create an object encapsulating the CLAHE parameters
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(50, 50))
+    img[:, :, 0] = clahe.apply(img[:, :, 0])
+
     # Create binary output mask
     binary_output = np.zeros_like(img[:, :, 0])
-    binary_output[(img[:, :, 2] >= thresh[0]) & (img[:, :, 2] <= thresh[1])] = 1
+    binary_output[(img[:, :, 0] >= thresh[0]) & (img[:, :, 0] <= thresh[1])] = 1
 
     return binary_output
 
@@ -51,12 +59,14 @@ def hsv_v_thresh(img, thresh=(0, 255)):
 
 if __name__ == "__main__":
     image = cv2.imread("./test_images/test6.jpg", cv2.IMREAD_UNCHANGED)
+    # imread reads in BGR. Convert to RGB
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    hls_thresh = hls_s_thresh(image, (120, 255))
-    rgb_thresh = rgb_r_thresh(image, (200, 255))
+    hls_thresh = hls_s_thresh(image, (180, 255))
+    rgb_thresh = rgb_r_thresh(image, (240, 255))
 
     hsv_h = hsv_h_thresh(image, (20, 40))
-    hsv_v = hsv_v_thresh(image, (220, 255))
+    hsv_v = hsv_v_thresh(image, (230, 255))
 
     fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3)
     ax1.imshow(hls_thresh, cmap="gray")
